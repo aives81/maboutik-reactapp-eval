b8a20594964dcaefd37d30e2784b8121b9461643
Yves-ROland <yvesrolanddebo@gmail.com>
2024-10-21 12:50:37 +0200

    Gestion de la suppression du panier

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,25 +7,34 @@ import '../assets/Product.css'; // Assure-toi d'importer le fichier CSS si tu en
 
 const Cart = () => {
     const cartItems = useCartStore(state => state.cartItems);
+    const removeFromCart = useCartStore(state => state.removeFromCart);
+
+    const totalAmount = cartItems.reduce((total, item) => total + item.price, 0);
 
     return (
-        <div className="cart product-list">
-            <h2>Mon Panier</h2>
-            {cartItems.length === 0 ? (
-                <p>Le panier est vide.</p>
-            ) : (
-                cartItems.map(item => (
-                    <div className="product-card">
-                        <img src={productImg} alt={item.name} className="product-image"/>
-                        <h2 className="product-name">{item.name}</h2>
-                        <p className="product-price">{item.price.toFixed(2)} €</p>
-                        <p className="product-gender">{item.gender}</p>
-                        <p className="product-description">{item.description}</p>
-                        <button className="remove-to-cart">Supprimer du panier</button>
-                    </div>
-                ))
-            )}
-        </div>
+        <>
+            <p>
+                <h2>Mon Panier</h2>
+                Total de mon panier: {totalAmount.toFixed(2)} €
+            </p>
+            <div className="cart product-list">
+                <br/>
+                {cartItems.length === 0 ? (
+                    <p>Le panier est vide.</p>
+                ) : (
+                    cartItems.map(item => (
+                        <div className="product-card">
+                            <img src={productImg} alt={item.name} className="product-image"/>
+                            <h2 className="product-name">{item.name}</h2>
+                            <p className="product-price">{item.price.toFixed(2)} €</p>
+                            <p className="product-gender">{item.gender}</p>
+                            <p className="product-description">{item.description}</p>
+                            <button className="remove-to-cart" onClick={() => removeFromCart(item.id)}>Supprimer du panier</button>
+                        </div>
+                    ))
+                )}
+            </div>
+        </>
     );
 };
 
